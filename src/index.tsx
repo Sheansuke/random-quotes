af14a9b2dbbb094e1896e048a6a8c9ac3dd0676a
Sheansuke <suerojean@gmail.com>
2020-09-21 03:53:21 -0400

    changes :D

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-import "./assets/main.css";
+import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
